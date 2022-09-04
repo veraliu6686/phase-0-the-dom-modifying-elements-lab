dd1dfbd0ae192f1d9685c85493675c50d1542fcf
lianzi liu <lianziliu.sugar@gmail.com>
2022-09-04 11:03:07 -0400

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.getElementById("main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id ="victory";
+newHeader.innerHTML = " Who is the champion";
