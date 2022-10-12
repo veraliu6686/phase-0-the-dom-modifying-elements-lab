4b9da2f6c91ad31c5723d4230b91c8080645a9d7
lianzi liu <lianziliu.sugar@gmail.com>
2022-10-12 08:43:06 -0400

    commit tree b7fb1024bd922b81e200fc75fbc380b9823cbc57

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ document.getElementById("main").remove();
 const newHeader = document.createElement("h1");
 newHeader.id ="victory";
 newHeader.innerHTML = " Who is the champion";
+
+const element = document.createElement("div");
+
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+    const li = document.createElement("li");
+    li.textContent = (i + 1).toString();
+    ul.append(li);
+  }
+
+  element.append(ul);
